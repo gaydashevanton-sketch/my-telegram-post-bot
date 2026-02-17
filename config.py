eb78e57541a0d9824c5c6f80628df67a14014e48
gaydashevanton-sketch <gaydashev.anton@gmail.com>
2026-02-17 18:52:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # для локальной разработки
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+# Проверяем, что ID получен и преобразуем в число
+if CHANNEL_ID is None:
+    raise ValueError("CHANNEL_ID не установлен в переменных окружения!")
+CHANNEL_ID = int(CHANNEL_ID)  # Telegram ожидает число, а не строку
